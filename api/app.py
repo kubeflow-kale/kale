58a469d5e28b4fe54765fd2b622a8a705fa5b75f
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2019-04-30 15:01:04 +0200

    Added random code to pipeline name to avoid deployment conflicts

--- api/app.py
+++ api/app.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_restful import reqparse
@@ -10,11 +13,17 @@ api = Api(app)
 
 
 class sumNumbers(Resource):
+
+    def random_string(self, string_length=10):
+        """Generate a random string of fixed length """
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for _ in range(string_length))
+
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('nb', type=str, help='Rate to charge for this resource')
         parser.add_argument('deploy', type=inputs.boolean, help='True to deploy the pipeline to a running KFP instance')
-        parser.add_argument('kfp_port', type=int, default=8080,
+        parser.add_argument('kfp_port', type=int, default=1234,
                             help='Local port map to remote KFP instance. KFP assumed to be at localhost:<port>/pipeline')
         parser.add_argument('pipeline_name', required=True, type=str, help='Name of the deployed pipeline')
         parser.add_argument('pipeline_descr', required=True, type=str, help='Description of the deployed pipeline')
@@ -35,7 +44,7 @@ class sumNumbers(Resource):
 
         Kale(
             source_notebook_path='./api/build/nb.ipynb',
-            pipeline_name=args['pipeline_name'],
+            pipeline_name=args['pipeline_name'] + "-" + self.random_string(4),
             pipeline_descr=args['pipeline_descr'],
             docker_image=args['docker_image'],
             auto_deploy=args['deploy'],
