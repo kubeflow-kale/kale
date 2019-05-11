37f830d6fa3bdfc0f36bda1d98e90009dd04d99c
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2019-05-11 12:43:08 +0200

    Fixes to description and image ref for public package

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='kfp-kale',
-    version='0.1',
+    version='0.1.2',
     description='Convert JupyterNotebooks to Kubeflow Pipelines deployments',
-    longdescription=readme(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/StefanoFioravanzo/kale',
     author='Stefano Fioravanzo',
     author_email='fioravanzo@fbk.eu',
