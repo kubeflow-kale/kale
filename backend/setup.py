7c29ffe018e49ff7d30961b668a531f534849ea3
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2019-09-22 17:01:37 +0200

    backend: Minor version bump

--- backend/setup.py
+++ backend/setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='kubeflow-kale',
-    version='0.2.1',
+    version='0.2.2',
     description='Convert JupyterNotebooks to Kubeflow Pipelines deployments',
     long_description=long_description,
     long_description_content_type='text/markdown',
