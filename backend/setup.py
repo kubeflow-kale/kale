4e203d8c4f95f4115f1963dbb6046983848cb946
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2019-10-07 15:45:19 +0100

    backend: Version bump to 0.3.3

--- backend/setup.py
+++ backend/setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='kubeflow-kale',
-    version='0.3.2',
+    version='0.3.3',
     description='Convert JupyterNotebooks to Kubeflow Pipelines deployments',
     long_description=long_description,
     long_description_content_type='text/markdown',
