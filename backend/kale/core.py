513bfaa5b7d33d766bdda58e0e838c5567147235
Kostis Lolos <klolos@arrikto.com>
2019-10-14 12:31:21 +0300

    backend: Support cloning existing notebook volumes
    Signed-off-by: Kostis Lolos <klolos@arrikto.com>

--- backend/kale/core.py
+++ backend/kale/core.py
@@ -1,10 +1,13 @@
 import os
 import re
+import copy
+import json
 import pprint
 import random
 import string
 
 import logging
+import subprocess
 import logging.handlers
 
 import nbformat as nb
@@ -12,14 +15,20 @@ import networkx as nx
 
 from pathlib import Path
 
+from kubernetes.config import ConfigException
+
 from kale.nbparser import parser
 from kale.static_analysis import dep_analysis
 from kale.codegen import generate_code
+from kale.utils.pod_utils import get_namespace, get_docker_base_image
 
-from kale.utils import pod_utils
 
-from kubernetes.config import ConfigException
+NOTEBOOK_SNAPSHOT_COMMIT_MESSAGE = """\
+This is a snapshot of notebook {} in namespace {}.
 
+This snapshot was created by Kale in order to clone the volumes of the notebook
+and use them to spawn a Kubeflow pipeline.\
+"""
 
 KALE_NOTEBOOK_METADATA_KEY = 'kubeflow_notebook'
 METADATA_REQUIRED_KEYS = [
@@ -37,8 +46,7 @@ class Kale:
     def __init__(self,
                  source_notebook_path: str,
                  notebook_metadata_overrides: dict = None,
-                 debug: bool = False
-                 ):
+                 debug: bool = False):
         self.source_path = Path(source_notebook_path)
         if not self.source_path.exists():
             raise ValueError(f"Path {self.source_path} does not exist")
@@ -82,20 +90,29 @@ class Kale:
         # mute other loggers
         logging.getLogger('urllib3.connectionpool').setLevel(logging.CRITICAL)
 
+        # Replace all requested cloned volumes with the snapshotted PVCs
+        volumes = self.pipeline_metadata['volumes'][:] \
+            if self.pipeline_metadata['volumes'] else []
+        self.pipeline_metadata['volumes'] = self.create_cloned_volumes(volumes)
+
     def validate_metadata(self):
         kale_block_name_regex = r'^[a-z0-9]([-a-z0-9]*[a-z0-9])?$'
-        kale_name_msg = "must consist of lower case alphanumeric characters or '-', " \
-                        "and must start and end with an alphanumeric character."
+        kale_name_msg = ("must consist of lower case alphanumeric characters "
+                         "or '-', and must start and end with an alphanumeric"
+                         " character.")
         k8s_valid_name_regex = r'^[\.\-a-z0-9]+$'
-        k8s_name_msg = "must consist of lower case alphanumeric characters, '-' or '.'"
+        k8s_name_msg = ("must consist of lower case alphanumeric characters, "
+                        "'-' or '.'")
 
         # check for required fields
         for required in METADATA_REQUIRED_KEYS:
             if required not in self.pipeline_metadata:
-                raise ValueError(
-                    "Key %s not found. Add this field either on the notebook metadata or as an override" % required)
+                raise ValueError("Key %s not found. Add this field either on "
+                                 "the notebook metadata or as an override" %
+                                 required)
 
-        if not re.match(kale_block_name_regex, self.pipeline_metadata['pipeline_name']):
+        if not re.match(kale_block_name_regex,
+                        self.pipeline_metadata['pipeline_name']):
             raise ValueError("Pipeline name  %s" % kale_name_msg)
 
         volumes = self.pipeline_metadata.get('volumes', [])
@@ -105,13 +122,79 @@ class Kale:
                     raise ValueError("Provide a valid name for every volume")
                 if not re.match(k8s_valid_name_regex, v['name']):
                     raise ValueError(f"PV/PVC resource name {k8s_name_msg}")
-                if 'snapshot' in v and v['snapshot'] and \
-                        (('snapshot_name' not in v) or not re.match(k8s_valid_name_regex, v['snapshot_name'])):
+                if ('snapshot' in v and
+                    v['snapshot'] and
+                    (('snapshot_name' not in v) or
+                     not re.match(k8s_valid_name_regex, v['snapshot_name']))):
                     raise ValueError(
-                        "Provide a valid snapshot resource name if you want to snapshot a volume. "
-                        "Snapshot resource name %s" % k8s_name_msg)
+                        "Provide a valid snapshot resource name if you want to"
+                        " snapshot a volume. Snapshot resource name %s" %
+                        k8s_name_msg)
         else:
             raise ValueError("Volumes must be a valid list of volumes spec")
+
+    def run_cmd(self, cmd):
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        exit_code = p.wait()
+        out = p.stdout.read()
+        err = p.stderr.read()
+        if exit_code:
+            msg = f"Command '{cmd}' failed with exit code: {exit_code}"
+            self.logger.error("{}:{}".format(msg, err))
+            raise RuntimeError(msg)
+
+        return out
+
+    def _get_cloned_volume(self, volume, snapshot_volumes):
+        for snap in snapshot_volumes:
+            if snap['mount_point'] == volume['mount_point']:
+                volume = copy.deepcopy(volume)
+                volume['type'] = 'new_pvc'
+                volume['annotation'] = {'key': 'rok/origin',
+                                        'value': snap['rok_url']}
+                return volume
+
+        msg = f"Volume '{volume['name']}' not found in notebook snapshot"
+        raise ValueError(msg)
+
+    def create_cloned_volumes(self, volumes):
+        if not any(v['type'] == 'clone' for v in volumes):
+            return volumes
+
+        # FIXME: Make sure the bucket exists
+        bucket_name = "notebooks"
+        hostname = os.getenv("HOSTNAME")
+        # FIXME: Import the Rok client instead of spawning external commands
+        namespace = get_namespace()
+        commit_title = f"Snapshot of notebook {hostname}"
+        commit_message = NOTEBOOK_SNAPSHOT_COMMIT_MESSAGE.format(hostname,
+                                                                 namespace)
+        output = self.run_cmd(f"rok-gw -o json object-register jupyter"
+                              f" '{bucket_name}' '{hostname}' --no-interactive"
+                              f" --param namespace='{namespace}'"
+                              f" --param commit_title='{commit_title}'"
+                              f" --param commit_message='{commit_message}'")
+
+        output = json.loads(output)
+        snapshot_volumes = output['result']['version']['group_members']
+
+        # Retrieve the mount point of each snapshotted volume
+        for v in snapshot_volumes:
+            obj_name = v["object_name"]
+            version_name = v["version_name"]
+            output = self.run_cmd(f"rok-gw -o json object-show '{bucket_name}'"
+                                  f" '{obj_name}' --version '{version_name}'"
+                                  " --detail")
+            v["mount_point"] = json.loads(output)["metadata"]["mountpoint"]
+
+        _volumes = []
+        for volume in volumes or []:
+            if volume['type'] == 'clone':
+                volume = self._get_cloned_volume(volume, snapshot_volumes)
+            _volumes.append(volume)
+
+        return _volumes
 
     def detect_environment(self):
         """
@@ -126,7 +209,7 @@ class Kale:
         if not self.pipeline_metadata['docker_image']:
             try:
                 # will fail in case in cluster config is not found
-                self.pipeline_metadata['docker_image'] = pod_utils.get_docker_base_image()
+                self.pipeline_metadata['docker_image'] = get_docker_base_image()
             except ConfigException:
                 # no K8s config found
                 # use kfp default image
