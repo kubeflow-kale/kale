2ba9102f1abab4776acaa1f89d3c1602a843dfb0
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2019-10-23 14:10:58 +0100

    backend: Moved deploy code to kfp_utils library

--- backend/kale/core.py
+++ backend/kale/core.py
@@ -1,7 +1,6 @@
 import os
 import re
 import pprint
-import tempfile
 
 import logging
 import logging.handlers
@@ -10,7 +9,6 @@ import nbformat as nb
 import networkx as nx
 
 from pathlib import Path
-from shutil import copyfile
 
 from kale.nbparser import parser
 from kale.static_analysis import dep_analysis
@@ -42,11 +40,14 @@ class Kale:
         self.notebook = nb.read(self.source_path.__str__(), as_version=nb.NO_CONVERT)
 
         # read Kale notebook metadata. In case it is not specified get an empty dict
-        notebook_metadata = self.notebook.metadata.get(KALE_NOTEBOOK_METADATA_KEY, dict())
+        self.pipeline_metadata = self.notebook.metadata.get(KALE_NOTEBOOK_METADATA_KEY, dict())
         # override notebook metadata with provided arguments
-        self.pipeline_metadata = {**notebook_metadata, **{k: v for k, v in vars(notebook_metadata_overrides).items() if v is not None}}
+        if notebook_metadata_overrides:
+            self.pipeline_metadata = {**self.pipeline_metadata,
+                                      **{k: v for k, v in notebook_metadata_overrides.items() if v}}
+
         self.validate_metadata()
-        self.detect_current_environment()
+        self.detect_environment()
 
         # setup logging
         self.logger = logging.getLogger("kubeflow-kale")
@@ -88,7 +89,7 @@ class Kale:
             raise ValueError("Pipeline name  %s" % kale_name_msg)
 
         volumes = self.pipeline_metadata.get('volumes', [])
-        if volumes and isinstance(volumes, list):
+        if volumes or isinstance(volumes, list):
             for v in volumes:
                 if 'name' not in v:
                     raise ValueError("Provide a valid name for every volume")
@@ -102,7 +103,7 @@ class Kale:
         else:
             raise ValueError("Volumes must be a valid list of volumes spec")
 
-    def detect_current_environment(self):
+    def detect_environment(self):
         """
         Detect local configs to preserve reproducibility of
         dev env in pipeline steps
@@ -152,61 +153,6 @@ class Kale:
         # save kfp generated code
         self.save_pipeline(kfp_code, output_path)
         return output_path
-
-    def run_func_form_notebook(self, func, **kwargs):
-        try:
-            return func(**kwargs)
-        except Exception as e:
-            # self.logger.debug(traceback.print_exc())
-            self.logger.debug(e, exc_info=True)
-            self.logger.error(e)
-            self.logger.error("To see full traceback run Kale with --debug flag or have a look at kale.log logfile")
-
-    def deploy_pipeline_to_kfp(self, pipeline_source, auto_upload=True, auto_run=False):
-        """
-        Import the generated kfp pipeline definition and deploy it
-        to a running KFP instance
-        """
-        import kfp.compiler as compiler
-        import kfp
-        import importlib.util
-
-        # create a tmp folder
-        tmp_dir = tempfile.mkdtemp()
-        # copy generated script to temp dir
-        copyfile(pipeline_source, tmp_dir + '/' + "pipeline_code.py")
-
-        spec = importlib.util.spec_from_file_location(tmp_dir.split('/')[-1], tmp_dir + '/' + 'pipeline_code.py')
-        foo = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(foo)
-
-        pipeline_package = self.pipeline_metadata['pipeline_name'] + '.pipeline.tar.gz'
-        compiler.Compiler().compile(foo.auto_generated_pipeline, pipeline_package)
-
-        try:
-            # Get or create an experiment and submit a pipeline run
-            # client = kfp.Client(host=self.kfp_url)
-            client = kfp.Client()
-
-            # upload the pipeline
-            if auto_upload:
-                client.upload_pipeline(pipeline_package, pipeline_name=self.pipeline_metadata['pipeline_name'])
-
-            if auto_run:
-                # create experiment or get existing one
-                experiment = client.create_experiment(self.pipeline_metadata['experiment_name'])
-                # Submit a pipeline run
-                run_name = self.pipeline_metadata['pipeline_name'] + '_run'
-                run = client.run_pipeline(experiment.id, run_name, pipeline_package, {})
-                run_link = f"http://{self.pipeline_metadata['kfp_dns']}/pipeline/#/runs/details/{run.id}"
-                self.logger.info(f"Deployment Successful. Check the pipeline run in the KFP UI.")
-        except Exception:
-            # remove auto-generated tar package (used for deploy)
-            os.remove(self.pipeline_metadata['pipeline_name'] + '.pipeline.tar.gz')
-            # raise again so excp is caught at higher level
-            raise
-
-        # sys.path.remove(tmp_dir)
 
     def print_pipeline(self, pipeline_graph):
         """
