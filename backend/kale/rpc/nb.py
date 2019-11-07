a70a8cdd687edd7aa53b299f1efdeb524c49ee3f
Ilias Katsakioris <elikatsis@arrikto.com>
2019-11-07 18:49:05 +0200

    backend: Enable notebook recovery
    * rpc.nb.resume_notebook_path()
    * rpc.nb.remove_marshal_dir
    * Marshal inside marshal directory, instead of
      'marshal_dir/pipeline_name'
    * Set marshal_dir to '.{nb_name}.kale.marshal.dir'
    * Every pipeline step creates the marshal directory, if it doesn't exist
    * Restore notebook when both {environment variables, marshal directory}
      exist
    * Once restoration is finished, remove the marshal directory
    
    Signed-off-by: Ilias Katsakioris <elikatsis@arrikto.com>
    Signed-off-by: Stefano Fioravanzo <stefano.fioravanzo@gmail.com>

--- backend/kale/rpc/nb.py
+++ backend/kale/rpc/nb.py
@@ -1,5 +1,33 @@
+import os
+import shutil
+
 from kale.core import Kale
 from kale.utils import pod_utils, kfp_utils
+from kale.marshal import resource_load
+
+
+KALE_MARSHAL_DIR_POSTFIX = ".kale.marshal.dir"
+KALE_PIPELINE_STEP_ENV = "KALE_PIPELINE_STEP"
+
+
+def resume_notebook_path():
+    p = os.environ.get("KALE_NOTEBOOK_PATH")
+    if p and not os.path.isfile(p):
+        raise RuntimeError("env path KALE_NOTEBOOK_PATH=%s is not a file" % p)
+    if not p:
+        return None
+
+    home = os.environ.get("HOME")
+    if not home.endswith('/'):
+        home = home + '/'
+
+    # JupyterLab needs a relative path to open a file
+    # JP should always run form the HOME directory, so we strip the
+    # leading HOME from the absolute path
+    if p.startswith(home):
+        return p[len(home):]
+    else:
+        return p
 
 
 def list_volumes():
@@ -31,3 +59,36 @@ def compile_notebook(source_notebook_path, notebook_metadata_overrides=None,
 
     return {"pipeline_package_path": package_path,
             "pipeline_metadata": instance.pipeline_metadata}
+
+
+def unmarshal_data(source_notebook_path):
+    kale_marshal_dir = ".%s%s" % (source_notebook_path,
+                                  KALE_MARSHAL_DIR_POSTFIX)
+    if not os.path.exists(kale_marshal_dir):
+        return {}
+
+    load_file_names = [f for f in os.listdir(kale_marshal_dir)
+                       if os.path.isfile(os.path.join(kale_marshal_dir, f))]
+
+    return {os.path.splitext(f)[0]:
+            resource_load(os.path.join(kale_marshal_dir, f))
+            for f in load_file_names}
+
+
+def explore_notebook(source_notebook_path):
+    step_name = os.getenv(KALE_PIPELINE_STEP_ENV, None)
+    kale_marshal_dir = ".%s%s" % (source_notebook_path,
+                                  KALE_MARSHAL_DIR_POSTFIX)
+
+    if step_name and os.path.exists(kale_marshal_dir):
+        return {"is_exploration": True,
+                "step_name": step_name}
+    return {"is_exploration": False,
+            "step_name": ""}
+
+
+def remove_marshal_dir(source_notebook_path):
+    kale_marshal_dir = ".%s%s" % (source_notebook_path,
+                                  KALE_MARSHAL_DIR_POSTFIX)
+    if os.path.exists(kale_marshal_dir):
+        shutil.rmtree(kale_marshal_dir)
