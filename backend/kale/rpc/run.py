eed8727217036ad4b8bc4b8fe61b8e0db4d10519
Ilias Katsakioris <elikatsis@arrikto.com>
2020-01-16 17:24:46 +0200

    backend: Setup RPC request object
    * Receive RPC context in rpc.run & sanitize it
    * Setup request in rpc.run containing a logger as well as the extras
      passed to the Adapter
    * Add request argument to RPC definitions (KaleRPCRequest class)
    * Rename global logger of RPC modules (log -> logger)
    * Make random_string() public and use it for trans_id generation
    
    Signed-off-by: Ilias Katsakioris <elikatsis@arrikto.com>

--- backend/kale/rpc/run.py
+++ backend/kale/rpc/run.py
@@ -2,14 +2,15 @@ import sys
 import logging
 import importlib
 
+from kale.core import random_string
 from kale.rpc import errors, utils
 from kale.rpc.log import create_adapter
 
 
-log = create_adapter(logging.getLogger(__name__))
+logger = create_adapter(logging.getLogger(__name__))
 
 
-def import_func(import_func_str):
+def import_func(request, import_func_str):
     """Import and return a function from a string."""
     mod_str, _sep, func_str = import_func_str.rpartition('.')
     if mod_str:
@@ -20,8 +21,8 @@ def import_func(import_func_str):
         mod = importlib.import_module(mod_str)
         return getattr(mod, func_str)
     except (ValueError, AttributeError) as e:
-        log.exception("Exception while importing function '%s' of module '%s':"
-                      " %s", func_str, mod_str, e)
+        request.log.exception("Exception while importing function '%s' of"
+                              " module '%s': %s", func_str, mod_str, e)
         raise ImportError("Function `%s' of module `%s' cannot be found: %s" %
                           (func_str, mod_str, e))
 
@@ -31,32 +32,67 @@ def format_success(result):
                             "result": result})
 
 
-def run(func, kwargs):
+class KaleRPCRequest(object):
+    """RPC request."""
+
+    def __init__(self, trans_id=None, nb_path=None):
+        if not trans_id:
+            trans_id = random_string(size=10)
+        self.log = create_adapter(logging.getLogger(__name__), trans_id,
+                                  nb_path)
+        self.trans_id = trans_id
+        self.nb_path = nb_path
+
+
+def sanitize_ctx(request, ctx):
+    """Keep just the known context fields."""
+    nb_path = ctx.pop("nb_path", None)
+    if ctx:
+        request.log.debug("Ignoring context fields: %s", ", ".join(ctx.keys()))
+    return {"nb_path": nb_path}
+
+
+def run(func, kwargs, ctx):
+    # Setup initial request obj to have something to log to
+    request = KaleRPCRequest()
+    request.log.debug("Decoding ctx of RPC function '%s'", func)
     try:
-        log.debug("Decoding kwargs of RPC function '%s'", func)
+        ctx = utils.deserialize(ctx)
+    except Exception:
+        exc_info = sys.exc_info()
+        request.log.exception("Failed to decode ctx: %s", ctx)
+        return errors.RPCEncodingError(message=str(exc_info[1])).serialize()
+    # Sanitize ctx and renew the request obj
+    ctx = sanitize_ctx(request, ctx)
+    request = KaleRPCRequest(request.trans_id, **ctx)
+
+    request.log.debug("Decoding kwargs of RPC function '%s'", func)
+    try:
         kwargs = utils.deserialize(kwargs)
     except Exception:
         exc_info = sys.exc_info()
-        log.exception("Failed to decode kwargs: %s", kwargs)
+        request.log.exception("Failed to decode kwargs: %s", kwargs)
         return errors.RPCEncodingError(message=str(exc_info[1])).serialize()
+
+    request.log.debug("Importing RPC function '%s'", func)
     try:
-        log.debug("Importing RPC function '%s'", func)
-        func = import_func(func)
+        func = import_func(request, func)
     except ImportError as e:
         exc_info = sys.exc_info()
-        log.exception("Failed to import RPC function '%s'", func)
+        request.log.exception("Failed to import RPC function '%s'", func)
         return errors.RPCImportError(message=str(e)).serialize()
 
+    request.log.info("Executing RPC function '%s(%s)'", func.__name__,
+                     ", ".join("%s=%s" % i for i in kwargs.items()))
     try:
-        log.info("Executing RPC function '%s(%s)'", func.__name__,
-                 ", ".join("%s=%s" % i for i in kwargs.items()))
-        result = func(**kwargs)
+        result = func(request, **kwargs)
         return format_success(result)
     except errors._RPCError as e:
-        log.exception("RPC function '%s' raised an RPCError", func.__name__)
+        request.log.exception("RPC function '%s' raised an RPCError",
+                              func.__name__)
         return e.serialize()
     except Exception:
         exc_info = sys.exc_info()
-        log.exception("RPC function '%s' raised an unhandled exception",
-                      func.__name__)
+        request.log.exception(("RPC function '%s' raised an unhandled"
+                               " exception"), func.__name__)
         return errors.RPCUnhandledError(message=str(exc_info[1])).serialize()
