750d5a77f7d340cdc90b823b32113a829b75ecef
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2020-01-30 14:45:38 +0000

    backend: Feature nbparser refactoring (#48)
    * Move pipeline plot function to core.py
    
    The plot function was not needed in parser.py.
    Moreover, it makes more sense to just produce a dot file representing
    the graph and letting the use produce a plot with whatever library is
    preferred.
    
    * Remove legacy fields from merge_code
    
    * Remove legacy tags
    
    * Remove _dotdict class
    
    * Remove legacy tags from tags parsing
    
    * Use copy.deepcopy instead of custom copy function
    
    * Improve notebook tags parsing and graph generation
    
    - Improve handling untagged cells using `prev_ste_name`
    - Handle global code block separaterly (imports and functions have their
    own lists)
    - Handle code blocks as list of strings and merge them at the end
    - Preserve ordering of functions coming after imports
    
    * Move static_analysis unit tests under unit_tests folder
    
    Better to keep all unittest under the same folder since Kale's module
    are composed of few source files. In this way there is less clutter.
    
    * Add tests for nbparser module
    
    * Style changes
    
    * Prevent prev tags without block tags
    
    * Fix multiple consecutive untagged cells not merging together
    
    Variable `prev_step_name` was assigned None every time an untagged cell
    was parsed.
    
    * Introduce `step` tag
    
    Tag`step` should become the standard replacemente to tag `block`, that
    will be deprecated in a future release.
    
    * Add a deprecation warning for block tag

--- backend/kale/nbparser/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-The input of this module is the tagged notebook
-and the output needs to be a networkx graph ready to be used
-with all the merged codes cells in the respective nodes and all
-the tags registered in the nodes
-
-The graph the needs to be passed to the static_analysis module
-"""
