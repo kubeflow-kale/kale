eb2078a176f87c5df2acb2386bc5d227b4e11cc9
Stefano Fioravanzo <stefano@arrikto.com>
2020-02-07 18:26:42 +0100

    Add function to get pipeline parameters

--- kale/nbparser/parser.py
+++ kale/nbparser/parser.py
@@ -105,6 +105,44 @@ def merge_code(nb_graph, dst, code):
     nx.set_node_attributes(nb_graph, {dst: {'source': source_code + [code]}})
 
 
+def get_pipeline_parameters_source(notebook):
+    """Get just pipeline parameters cells from the notebook.
+
+    Args (nbformat.notebook): Notebook object
+
+    Returns (str): pipeline parameters source code
+    """
+    detected = False
+    pipeline_parameters = ''
+    for c in notebook.cells:
+        # parse only source code cells
+        if c.cell_type != "code":
+            continue
+        # in case the previous cell was a pipeline-parameter cell and this
+        # cell either:
+        #  - does not have tags
+        #  - does not have any `block` or `step` tags
+        #  - is not a skip tag
+        if ((('tags' not in c.metadata
+              or len(c.metadata['tags']) == 0)
+             or (not any(re.match(BLOCK_TAG, t) for t in c.metadata['tags'])
+                 and not any(re.match(STEP_TAG, t)
+                             for t in c.metadata['tags'])
+                 and not any(re.match(SKIP_TAG, t)
+                             for t in c.metadata['tags'])))
+                and detected):
+            pipeline_parameters += '\n' + c.source
+        elif (('tags' in c.metadata
+               and len(c.metadata['tags']) > 0
+               and any(re.match(PIPELINE_PARAMETERS_TAG, t)
+                       for t in c.metadata['tags']))):
+            pipeline_parameters += '\n' + c.source
+            detected = True
+        else:
+            detected = False
+    return pipeline_parameters.strip()
+
+
 def parse_notebook(notebook):
     """Creates a NetworkX graph based on the input notebook's tags.
 
