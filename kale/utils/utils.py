89c0a9a2839885cba526f396e3d8f3189c411ffb
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2020-02-11 23:52:18 +0200

    Add e2e test from Titanic notebook to Python DSL (#56)
    * Move functions test assets to functions folder
    
    * Remove old tests notebook folder
    
    * Extend save_pipeline function to save to tmp
    
    * Move abs working dir to utils to make it testable
    
    * Sort `ins` and `outs` pipeline graph properties
    
    Sorting is needed in order to ensure reproducibility. Since `ins` and
    `outs` are treated as sets (to perform inter-sets operations) there is
    no ordering guarantee. Without sorting, the generated pipeline would
    generate the saving and loading code in random order.
    
    * Add e2e test
    
    * Python 3.5 compatibility

--- kale/utils/utils.py
+++ kale/utils/utils.py
@@ -1,3 +1,4 @@
+import os
 import random
 import string
 
@@ -5,3 +6,8 @@ import string
 def random_string(size=5, chars=string.ascii_lowercase + string.digits):
     """Generate random string."""
     return "".join(random.choice(chars) for _ in range(size))
+
+
+def get_abs_working_dir(path):
+    """Get absolute path to parent dir."""
+    return os.path.dirname(os.path.abspath(path))
