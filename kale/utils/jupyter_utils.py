b11f8c3cec8c33e27cd3de10b9c9b14339a1f779
Stefano Fioravanzo <stefano@arrikto.com>
2020-02-28 12:19:15 +0200

    Capture cell outputs and generate html artifacts

--- kale/utils/jupyter_utils.py
+++ kale/utils/jupyter_utils.py
@@ -1,18 +1,178 @@
 import os
 import sys
+import json
 import signal
 import nbformat
 import threading
 
 from queue import Empty
+from kale.utils import pod_utils
 from jupyter_client.kernelspec import get_kernel_spec
 from kale.utils.utils import remove_ansi_color_sequences
 from nbconvert.preprocessors.execute import ExecutePreprocessor
+
+html_template = '''
+<html><head>
+    <style>
+        table {
+            border: none;
+            border-collapse: collapse;
+            border-spacing: 0;
+            font-size: 14px;
+        }
+        td,
+        th {
+            text-align: right;
+            vertical-align: middle;
+            padding: 0.5em 0.5em;
+            line-height: 1.0;
+            white-space: nowrap;
+            max-width: 100px;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            border: none;
+        }
+        th {
+            font-weight: bold;
+        }
+        tbody tr:nth-child(odd) {
+            background: rgb(245, 245, 245);
+        }
+    </style>
+</head>
+<body><div>
+    %s
+</div></body>
+</html>
+'''
+
+image_html_template = '''
+<div>
+  <p>%s</p>
+  <img src="data:image/png;base64, %s" />
+</div>
+'''
+
+text_html_template = '''
+<pre>
+------- CELL OUTPUT -------
+%s
+---------------------------
+</pre>
+<br><br>
+'''
+
+javascript_html_template = '''
+<script>
+%s
+</script>
+'''
 
 
 class KaleKernelException(Exception):
     """Raised when signal_handler receives a signal from capture_streams."""
     pass
+
+
+def generate_html_output(outputs):
+    """Transform a notebook cell rich outputs into a html page.
+
+    Args:
+        outputs: notebook cell output
+
+    Returns: html multiline string
+    """
+    if not isinstance(outputs, list):
+        raise ValueError("A notebook's cell outputs must be a valid list."
+                         " Found %s instead." % type(outputs))
+    html_body = ""
+    # run through the list of outputs
+    for o in outputs:
+        # the only rich outputs should come from `display_data` and
+        # `execution_result` messages. The latter are identical to
+        # `display_data` messages, with the addition of an execution_count key.
+        output_type = o.get('output_type', None)
+        if not output_type:
+            raise ValueError("Cell output dict has not `output_type` field."
+                             " Output: %s" % o)
+        if o['output_type'] in ['display_data', 'execute_result']:
+            # check mime-type of content
+            # Currently supported MIME types:
+            # see: https://ipython.org/ipython-doc/2/api/generated/IPython.core.displaypub.html#IPython.core.displaypub.DisplayPublisher
+            # text / plain
+            # text / html
+            # text / markdown
+            # text / latex
+            # application / json
+            # application / javascript
+            # image / png
+            # image / jpeg
+            # image / svg + xml
+            data = o['data']
+            # TODO: Generalize to multiple image types (i.e. jpeg and svg+xml)
+            if 'image/png' in data:
+                title = data.get('text/plain', '')
+                html = image_html_template % (title, data['image/png'])
+                html_body += html
+
+            if 'text/html' in data:
+                html_body += data['text/html']
+
+            if 'image/png' not in data \
+                    and 'text/html' not in data \
+                    and 'text/plain' in data:
+                html_body += text_html_template % data['text/plain']
+
+            if 'application/javascript' in data:
+                html_body += javascript_html_template \
+                             % data['application/javascript']
+    return html_body
+
+
+def update_uimetadata(artifact_name,
+                      uimetadata_path='/mlpipeline-ui-metadata.json'):
+    """Update ui-metadata dictionary with a new web-app entry.
+
+    Args:
+        artifact_name: Name of the artifact
+        uimetadata_path: path to mlpipeline-ui-metadata.json
+    """
+    # Default empty ui-metadata dict
+    outputs = {"outputs": []}
+    if os.path.exists(uimetadata_path):
+        try:
+            outputs = json.loads(
+                open(uimetadata_path, 'r').read())
+            if not outputs.get('outputs', None):
+                outputs['outputs'] = []
+        except json.JSONDecodeError as e:
+            print("Failed to parse json file %s: %s\n"
+                  "This step will not be able to visualize artifacts in the"
+                  " KFP UI" % uimetadata_path, e)
+
+    pod_name = pod_utils.get_pod_name()
+    namespace = pod_utils.get_namespace()
+    workflow_name = pod_utils.get_workflow_name(pod_name, namespace)
+    html_artifact_entry = [{
+        'type': 'web-app',
+        'storage': 'minio',
+        'source': 'minio://mlpipeline/artifacts/%s/%s/%s' % (
+            workflow_name, pod_name, artifact_name + '.tgz')
+    }]
+    outputs['outputs'] += html_artifact_entry
+    with open(uimetadata_path, "w") as f:
+        json.dump(outputs, f)
+
+
+def process_outputs(cells):
+    """Process a list of cells outputs after execution."""
+    html_outputs = [generate_html_output(c.outputs)
+                    for c in cells]
+    html_outputs = '\n'.join(html_outputs).strip()
+    if html_outputs == "":
+        html_outputs = "This step did not produce any artifacts."
+    html_artifact = html_template % html_outputs
+    return html_artifact
 
 
 def capture_streams(kc, exit_on_error=False, timeout=10):
@@ -120,3 +280,6 @@ def run_code(source: tuple, kernel_name='python3'):
     except KaleKernelException:
         # exit gracefully with error
         sys.exit(-1)
+
+    result = process_outputs(notebook.cells)
+    return result
