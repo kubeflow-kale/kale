7d028444f6bdd02200690729864cd770a768e91b
Stefano Fioravanzo <stefano@arrikto.com>
2020-03-09 13:34:33 +0200

    Format test_generate_code.py

--- kale/tests/unit_tests/test_generate_code.py
+++ kale/tests/unit_tests/test_generate_code.py
@@ -28,45 +28,39 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         'size': 5,
         'type': 'pv',
         'mount_point': '/'
-    }],
-     ({})),
+    }], ({})),
     # ---
     ([{
         'name': 'test_volume',
         'type': 'pvc',
         'mount_point': '/'
-    }],
-     {'vol_': ('str', 'test_volume')}),
+    }], {'vol_': ('str', 'test_volume')}),
     # ---
     ([{
         'name': 'test_volume',
         'type': 'pvc',
         'mount_point': '/root'
-    }],
-     {'vol_root': ('str', 'test_volume')}),
+    }], {'vol_root': ('str', 'test_volume')}),
     # ---
     ([{
         'name': 'test_volume',
         'type': 'pvc',
         'mount_point': '/root/user/'
-    }],
-     {'vol_root_user': ('str', 'test_volume')}),
+    }], {'vol_root_user': ('str', 'test_volume')}),
     # ---
     ([{
         'name': 'test_volume',
         'type': 'new_pvc',
         'mount_point': '/',
         'annotations': {}
-    }],
-     {}),
+    }], {}),
     # ---
     ([{
         'name': 'test-volume',
         'type': 'new_pvc',
         'mount_point': '/root',
         'annotations': {'rok/origin': 'url'}
-    }],
-     {'rok_test_volume_url': ('str', 'url')}),
+    }], {'rok_test_volume_url': ('str', 'url')}),
 ])
 def test_get_volumes_parameters(volumes, target):
     """Tests that volumes are correctly converted from list into dict."""
@@ -125,16 +119,17 @@ def _test_args_dict(f1, f2, f3):
      _test_args_dict(["param1"], ["int"], [1])),
     # ---
     ({'p1': ('int', 1), 'p2': ('str', 'v'), 'p3': ('bool', True)},
-     _test_args_dict(["p1", "p2", "p3"], ["int", "str", "bool"], [1, "v", True])),
+     _test_args_dict(["p1", "p2", "p3"], ["int", "str", "bool"],
+                     [1, "v", True])),
 ])
 def test_get_args(pipeline_parameters, target):
-    """Test that argument string are properly formatted"""
+    """Test that argument string are properly formatted."""
     assert target == generate_code.get_args(pipeline_parameters)
 
 
 @pytest.fixture(scope='module')
 def template():
-    """Reusable function template"""
+    """Reusable function template."""
     tmpl_dir = os.path.join(THIS_DIR, '../../templates')
     env = generate_code._initialize_templating_env(tmpl_dir)
     return env.get_template('function_template.jinja2')
@@ -160,7 +155,7 @@ def template():
 ])
 def test_generate_function(template, step_name, source, ins, outs, nb_path,
                            metadata, target):
-    """Test python code is generated correctly"""
+    """Test python code is generated correctly."""
     component_data = {'source': source, 'ins': ins, 'outs': outs}
     res = generate_code.generate_lightweight_component(template,
                                                        step_name,
