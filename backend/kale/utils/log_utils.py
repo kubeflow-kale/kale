cc265c464d19dca26e392680084a82ad7845107b
Ilias Katsakioris <elikatsis@arrikto.com>
2020-05-04 14:35:17 +0300

    log_utils: Add basic support for adding a file handler

--- backend/kale/utils/log_utils.py
+++ backend/kale/utils/log_utils.py
@@ -12,27 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import logging
 
+LOG_FMT = "%(asctime)s {} [%(levelname)s] %(message)s"
 _loggers = dict()
 
 
-def get_or_create_logger(module, name=None, level=logging.INFO):
-    """Get or create and return module's logger."""
+def _configure_handler(handler, level, formatter):
+    handler.setLevel(level)
+    handler.setFormatter(formatter)
+
+
+def get_or_create_logger(module, name=None, level=logging.INFO, fmt=None,
+                         file_level=logging.DEBUG, file_fmt=None,
+                         log_path=None):
+    """Get or create and return module's logger.
+
+    Args:
+        module: the module for which we request a logger
+        name: the name/title of the logger if default format is used
+        level: logging level for StreamHandlers
+        fmt: override default format
+        file_level: logging level for FileHandlers
+        file_fmt: override default file format
+        log_path: logfile path (if None, then no FileHandler is created)
+    """
     global _loggers
     log = _loggers.get(module)
     if log:
         for h in log.handlers:
-            h.setLevel(level)
+            # XXX: StreamHandler is FileHandler's parent class, so the order
+            # XXX: should be like this
+            if isinstance(h, logging.FileHandler):
+                h.setLevel(file_level)
+            elif isinstance(h, logging.StreamHandler):
+                h.setLevel(level)
         return log
-
-    # Setup handler
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(level)
-    stream_handler.setFormatter(logging.Formatter(
-        "%(asctime)s {} [%(levelname)s] %(message)s".format(name
-                                                            or "%(module)s"),
-        "%H:%M:%S"))
 
     # Setup logger
     log = logging.getLogger(module)
@@ -40,9 +56,23 @@ def get_or_create_logger(module, name=None, level=logging.INFO):
     # modules without multiple log messages with the same information
     log.propagate = False
     # Set DEBUG level for the root logger and let handlers set their own level
-    # XXX: In the future we may add the option for file handlers as well and
-    # XXX: get rid of some of the special logic in kale.rpc.log
     log.setLevel(logging.DEBUG)
+
+    # Setup handlers
+    log_fmt = fmt or LOG_FMT.format(name or "%(module)s:%(lineno)d")
+    stream_handler = logging.StreamHandler()
+    _configure_handler(stream_handler, level, logging.Formatter(log_fmt,
+                                                                "%H:%M:%S"))
     log.addHandler(stream_handler)
+
+    if log_path:
+        os.makedirs(os.path.dirname(log_path), exist_ok=True)
+        file_handler = logging.FileHandler(filename=os.path.abspath(log_path),
+                                           mode='a')
+        _configure_handler(file_handler, file_level,
+                           logging.Formatter(file_fmt or log_fmt,
+                                             "%Y-%m-%d %H:%M:%S"))
+        log.addHandler(file_handler)
+
     _loggers[module] = log
     return log
