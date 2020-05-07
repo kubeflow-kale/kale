kubeflow-kale/kale

commit 51a54ba9db0946ee8afdecfb965ab6bf6712fceb

Fix compiled artifact save paths (#117)
* Fix pipeline package export path to be next to the notebook

* Compile pipeline into YAML

* Fix Katib experiment YAML definition export path

* rpc/katib: (minor) Fix trans_id passing

Signed-off-by: Ilias Katsakioris <elikatsis@arrikto.com>
Reviewed-by: Stefano Fioravanzo <stefano@arrikto.com>

backend/kale/utils/kfp_utils.py
@@ -96,8 +96,10 @@ def compile_pipeline(pipeline_source, pipeline_name):
     spec.loader.exec_module(foo)
 
     # path to generated pipeline package
-    pipeline_package = pipeline_name + '.pipeline.tar.gz'
-    Compiler().compile(foo.auto_generated_pipeline, pipeline_package)
+    pipeline_package = pipeline_name + '.pipeline.yaml'
+    Compiler().compile(foo.auto_generated_pipeline,
+                       os.path.join(os.path.dirname(pipeline_source),
+                                    pipeline_package))
     return pipeline_package
 
 

labextension/src/components/LeftPanelWidget.tsx
@@ -272,6 +272,7 @@ interface IRunPipelineArgs {
 interface IKatibRunArgs {
   pipeline_id: string;
   pipeline_metadata: any;
+  output_path: string;
 }
 
 export interface IKatibExperiment {
@@ -1250,6 +1251,7 @@ export class KubeflowKaleLeftPanel extends React.Component<IProps, IState> {
             ...metadata,
             experiment_name: kfpExperiment.name,
           },
+          output_path: nbFilePath.substring(0, nbFilePath.lastIndexOf('/')),
         };
         let katibExperiment: IKatibExperiment = null;
         try {

backend/kale/core.py
@@ -154,7 +154,7 @@ class Kale:
             notebook_dir = os.path.dirname(self.source_path)
             filename = "{}.kale.py".format(
                 self.pipeline_metadata['pipeline_name'])
-            output_path = os.path.join(notebook_dir, filename)
+            output_path = os.path.abspath(os.path.join(notebook_dir, filename))
         # save kfp generated code
         output_path = self.save_pipeline(kfp_code, output_path)
         return output_path

backend/kale/rpc/katib.py
@@ -11,6 +11,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+import os
 import kubernetes
 from kubernetes.client.rest import ApiException
 
@@ -54,11 +56,8 @@ spec:
                 )"
 """
 
-trans_id = None
 
-
-def _get_k8s_co_client():
-    global trans_id
+def _get_k8s_co_client(trans_id):
     try:
         kubernetes.config.load_incluster_config()
     except Exception:  # Not in a notebook server
@@ -91,7 +90,7 @@ def _define_katib_experiment(name, katib_spec, trial_parameters):
 
 def _launch_katib_experiment(request, katib_experiment, namespace):
     """Launch Katib experiment."""
-    k8s_co_client = _get_k8s_co_client()
+    k8s_co_client = _get_k8s_co_client(request.trans_id)
 
     co_group = "kubeflow.org"
     co_version = "v1alpha3"
@@ -134,7 +133,8 @@ def _sanitize_katib_spec(request, katib_spec):
                                 KATIB_DEFAULTS, "Katib")
 
 
-def create_katib_experiment(request, pipeline_id, pipeline_metadata):
+def create_katib_experiment(request, pipeline_id, pipeline_metadata,
+                            output_path):
     """Create and launch a new Katib experiment.
 
     The Katib metadata must include all the information required to create an
@@ -146,11 +146,10 @@ def create_katib_experiment(request, pipeline_id, pipeline_metadata):
         request: RPC request object
         pipeline_id: The id of the KFP pipeline that will be run by the Trials
         pipeline_metadata: The Kale notebook metadata
+        output_path: The directory to store the YAML definition
 
     Returns (dict): a dictionary describing the status of the experiment
     """
-    global trans_id
-    trans_id = request.trans_id
     try:
         namespace = pod_utils.get_namespace()
     except Exception:
@@ -177,7 +176,11 @@ def create_katib_experiment(request, pipeline_id, pipeline_metadata):
 
     katib_experiment = _define_katib_experiment(katib_name, katib_spec,
                                                 trial_parameters)
-    with open("./%s.yaml" % katib_name, "w") as yaml_file:
+    definition_path = os.path.abspath(
+        os.path.join(output_path, "%s.katib.yaml" % katib_name))
+    request.log.info("Saving Katib experiment definition at %s",
+                     definition_path)
+    with open(definition_path, "w") as yaml_file:
         import yaml
         yaml_text = yaml.dump(katib_experiment)
         yaml_file.write(yaml_text)
@@ -203,7 +206,7 @@ def get_experiment(request, experiment, namespace):
 
     Returns (dict): a dict describing the status of the running experiment
     """
-    k8s_co_client = _get_k8s_co_client()
+    k8s_co_client = _get_k8s_co_client(request.trans_id)
 
     co_group = "kubeflow.org"
     co_version = "v1alpha3"

backend/kale/tests/unit_tests/test_rpc_katib.py
@@ -76,7 +76,7 @@ def test_create_katib_experiment():
     m = mock.mock_open()
     with mock.patch("builtins.open", m):
         katib.create_katib_experiment(_get_requets(), "12345",
-                                      pipeline_metadata)
+                                      pipeline_metadata, output_path=".")
 
     handle = m()
     target = open(os.path.join(THIS_DIR,

commit 4b830b745737b4c402c5d8466332e358a21f0356

Fix errors introduced by #117 (#119)
* Fix pipeline package path returned when compiling

* Fix link to pipeline definition file in progress panel

Signed-off-by: Ilias Katsakioris <elikatsis@arrikto.com>
Reviewed-by: Stefano Fioravanzo <stefano@arrikto.com>

backend/kale/rpc/nb.py
@@ -87,7 +87,7 @@ def compile_notebook(request, source_notebook_path,
     pipeline_name = instance.pipeline_metadata["pipeline_name"]
     package_path = kfp_utils.compile_pipeline(script_path, pipeline_name)
 
-    return {"pipeline_package_path": package_path,
+    return {"pipeline_package_path": os.path.relpath(package_path),
             "pipeline_metadata": instance.pipeline_metadata}
 
 

backend/kale/utils/kfp_utils.py
@@ -96,10 +96,9 @@ def compile_pipeline(pipeline_source, pipeline_name):
     spec.loader.exec_module(foo)
 
     # path to generated pipeline package
-    pipeline_package = pipeline_name + '.pipeline.yaml'
-    Compiler().compile(foo.auto_generated_pipeline,
-                       os.path.join(os.path.dirname(pipeline_source),
-                                    pipeline_package))
+    pipeline_package = os.path.join(os.path.dirname(pipeline_source),
+                                    pipeline_name + '.pipeline.yaml')
+    Compiler().compile(foo.auto_generated_pipeline, pipeline_package)
     return pipeline_package
 
 

labextension/src/widgets/LeftPanelWidget.tsx
@@ -1145,13 +1145,10 @@ export class KubeflowKaleLeftPanel extends React.Component<IProps, IState> {
     // workdir. Instead, the python script has a slightly different name and is
     // generated in the same directory where the notebook lives.
     this.updateDeployProgress(_deployIndex, {
-      compiledPath:
-        nbFilePath.substring(0, nbFilePath.lastIndexOf('/')) +
-        '/' +
-        compileNotebook.pipeline_package_path.replace(
-          'pipeline.tar.gz',
-          'kale.py',
-        ),
+      compiledPath: compileNotebook.pipeline_package_path.replace(
+        'pipeline.yaml',
+        'kale.py',
+      ),
     });
 
     // UPLOAD