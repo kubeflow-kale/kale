26bbdd9bfd9c69908d534a8675a5df0948fee762
Ilias Katsakioris <elikatsis@arrikto.com>
2020-07-31 18:19:29 +0300

    backend: Rename mlmd_utils to mlmdutils
    Signed-off-by: Ilias Katsakioris <elikatsis@arrikto.com>
    Reviewed-by: Stefano Fioravanzo <stefano@arrikto.com>

--- backend/kale/tests/assets/functions/func07.out.py
+++ backend/kale/tests/assets/functions/func07.out.py
@@ -1,14 +1,14 @@
 def final_auto_snapshot():
-    from kale.utils import mlmd_utils as _kale_mlmd_utils
-    _kale_mlmd_utils.init_metadata()
+    from kale.utils import mlmdutils as _kale_mlmdutils
+    _kale_mlmdutils.init_metadata()
 
     from kale.utils import podutils as _kale_podutils
-    _kale_mlmd_utils.call("link_input_rok_artifacts")
+    _kale_mlmdutils.call("link_input_rok_artifacts")
     _rok_snapshot_task = _kale_podutils.snapshot_pipeline_step(
         "T",
         "final_auto_snapshot",
         "/path/to/nb",
         before=False)
-    _kale_mlmd_utils.call("submit_output_rok_artifact", _rok_snapshot_task)
+    _kale_mlmdutils.call("submit_output_rok_artifact", _rok_snapshot_task)
 
-    _kale_mlmd_utils.call("mark_execution_complete")
+    _kale_mlmdutils.call("mark_execution_complete")
