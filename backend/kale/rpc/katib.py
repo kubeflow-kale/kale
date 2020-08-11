697370528696066fae82eb74f2624232f35d2fed
Ilias Katsakioris <elikatsis@arrikto.com>
2020-08-11 17:18:07 +0300

    backend: Allow Katib trials to access KFP
    * Add label for PodDefault to be applied
    * Specify service account for the trial
    * Bump image
      **TODO:** Create & use new image after merging
    
    Signed-off-by: Ilias Katsakioris <elikatsis@arrikto.com>
    Reviewed-by: Stefano Fioravanzo <stefano@arrikto.com>

--- backend/kale/rpc/katib.py
+++ backend/kale/rpc/katib.py
@@ -38,8 +38,11 @@ spec:
     metadata:
       annotations:
         sidecar.istio.io/inject: "false"
+      labels:
+        access-ml-pipeline: "true"
     spec:
       restartPolicy: Never
+      serviceAccountName: pipeline-runner
       containers:
         - name: {{.Trial}}
           image: {image}
@@ -169,7 +172,7 @@ def create_katib_experiment(request, pipeline_id, pipeline_metadata,
     katib_spec = _sanitize_katib_spec(request, katib_spec)
 
     trial_parameters = {
-        "image": "gcr.io/arrikto/katib-kfp-trial:acbe872",
+        "image": "gcr.io/arrikto/katib-kfp-trial:f1c9b6a",
         "pipeline_id": pipeline_id,
         "experiment_name": pipeline_metadata.get(
             "experiment_name")}
