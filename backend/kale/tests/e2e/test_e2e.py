8db355840c86be098e613e861241de5befe7a67f
Stefano Fioravanzo <stefano@arrikto.com>
2020-11-03 17:57:20 +0100

    sdk: Add e2e tests
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    Reviewed-by: Ilias Katsakioris <elikatsis@arrikto.com>

--- backend/kale/tests/e2e/test_e2e.py
+++ backend/kale/tests/e2e/test_e2e.py
@@ -14,6 +14,7 @@
 
 import os
 import pytest
+import argparse
 
 from unittest import mock
 
@@ -42,4 +43,41 @@ def test_notebook_to_dsl(random_string, notebook_path, dsl_path):
 
     expected_result = open(dsl_path).read()
     result = open(dsl_script_path).read()
+    assert result == expected_result
+
+
+_prfx = "kale.sdk.api."
+
+
+@pytest.mark.parametrize("py_path,dsl_path", [
+    (os.path.join(THIS_DIR, "../assets/sdk/", "simple_data_passing.py"),
+     os.path.join(THIS_DIR, "../assets/kfp_dsl/", "simple_data_passing.py")),
+    (os.path.join(THIS_DIR, "../assets/sdk/", "pipeline_parameters.py"),
+     os.path.join(THIS_DIR, "../assets/kfp_dsl/", "pipeline_parameters.py")),
+])
+@mock.patch("kale.pipeline.utils.abs_working_dir")
+@mock.patch(_prfx + "utils.main_source_lives_in_cwd")
+@mock.patch(_prfx + "_parse_cli_args")
+@mock.patch("kale.common.utils.random_string")
+def test_python_to_dsl(random_string,
+                       _parse_cli_args,
+                       main_source_lives_in_cwd,
+                       abs_working_dir,
+                       py_path,
+                       dsl_path):
+    """Test code generation enf to enf from python sdk to DSL."""
+    random_string.return_value = "test"
+    _parse_cli_args.return_value = argparse.Namespace(kfp=True, dry_run=True)
+    main_source_lives_in_cwd.return_value = True
+    abs_working_dir.return_value = "/test"
+
+    import importlib.util
+    spec = importlib.util.spec_from_file_location("test_kfp", py_path)
+    foo = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(foo)
+    script_path = foo.mypipeline()
+
+    expected_result = open(dsl_path).read()
+    result = open(script_path).read()
+    os.remove(script_path)
     assert result == expected_result
