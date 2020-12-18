6be1b976308c4e0461217b4b6e7c3e8fedce5ffb
Ilias Katsakioris <elikatsis@arrikto.com>
2020-12-18 22:29:52 +0200

    backend: Add function to construct a v1beta1 Katib experiment
    Signed-off-by: Ilias Katsakioris <elikatsis@arrikto.com>
    Reviewed-by: Stefano Fioravanzo <stefano@arrikto.com>

--- backend/kale/common/katibutils.py
+++ backend/kale/common/katibutils.py
@@ -14,17 +14,49 @@
 
 """Suite of helpers for Katib."""
 
+import copy
+
 from kale.common import k8sutils
 
 
 KATIB_API_GROUP = "kubeflow.org"
 KATIB_API_VERSION = "v1alpha3"
+KATIB_API_VERSION_V1BETA1 = "v1beta1"
 KATIB_TRIALS_PLURAL = "trials"
 
 EXPERIMENT_NAME_ANNOTATION_KEY = "kubeflow.org/katib-experiment-name"
 EXPERIMENT_ID_ANNOTATION_KEY = "kubeflow.org/katib-experiment-id"
 TRIAL_NAME_ANNOTATION_KEY = "kubeflow.org/katib-trial-name"
 TRIAL_ID_ANNOTATION_KEY = "kubeflow.org/katib-trial-id"
+
+TRIAL_IMAGE = "gcr.io/arrikto/katib-kfp-trial:dc982fe-d9bf99ac"
+TRIAL_SA = "pipeline-runner"
+TRIAL_CONTAINER_NAME = "main"
+KALE_PARAM_TRIAL_NAME = "kaleParamTrialName"
+
+# In the Job CR we can only use '${trialParameters.*}', so to access
+# information from the Trial spec (e.g. the Trial name) we need to first create
+# a parameter that can be referenced in the Job spec.
+TRIAL_PARAMETERS_BASE = [{"name": KALE_PARAM_TRIAL_NAME,
+                          "reference": "${trialSpec.Name}"}]
+
+JOB_CMD = """\
+python3 -u -c "from kale.common.kfputils import create_and_wait_kfp_run;\
+               create_and_wait_kfp_run(%s)"\
+"""
+JOB_CR = {"apiVersion": "batch/v1",
+          "kind": "Job",
+          "spec": {"backoffLimit": 0,
+                   "template": {
+                       "metadata": {
+                           "annotations": {"sidecar.istio.io/inject": "false"},
+                           "labels": {"access-ml-pipeline": "true"},
+                       },
+                       "spec": {"restartPolicy": "Never",
+                                "serviceAccountName": TRIAL_SA,
+                                "containers": [{"name": TRIAL_CONTAINER_NAME,
+                                                "image": TRIAL_IMAGE}]}
+                   }}}
 
 
 def annotate_trial(name, namespace, annotations):
@@ -68,3 +100,34 @@ def get_owner_experiment_from_trial(trial):
                            " Name: %s, Namespace: %s UID: %s"
                            % (trial_name, trial_namespace, trial_uid))
     return owner.get("name"), owner.get("uid")
+
+
+def construct_experiment_cr(name, experiment_spec, pipeline_id, version_id,
+                            experiment_name):
+    """Return an Experiment provided its spec."""
+    trial_tmpl = {"retain": True,
+                  "primaryContainerName": TRIAL_CONTAINER_NAME,
+                  "trialParameters": copy.deepcopy(TRIAL_PARAMETERS_BASE)}
+
+    spec = copy.deepcopy(experiment_spec)
+    param_names = [p["name"] for p in spec["parameters"]]
+    trial_tmpl["trialParameters"].extend([{"name": p, "reference": p}
+                                          for p in param_names])
+
+    kwargs = {p: "${trialParameters.%s}" % p for p in param_names}
+    kwargs.update({"pipeline_id": pipeline_id,
+                   "version_id": version_id,
+                   "run_name": "${trialParameters.%s}" % KALE_PARAM_TRIAL_NAME,
+                   "experiment_name": experiment_name})
+    cmd = JOB_CMD % ", ".join("%s='%s'" % (k, v) for k, v in kwargs.items())
+    job = copy.deepcopy(JOB_CR)
+    job["spec"]["template"]["spec"]["containers"][0]["command"] = [cmd]
+    trial_tmpl["trialSpec"] = job
+    spec["trialTemplate"] = trial_tmpl
+    experiment = {"apiVersion": "%s/%s" % (KATIB_API_GROUP,
+                                           KATIB_API_VERSION_V1BETA1),
+                  "kind": "Experiment",
+                  "metadata": {"name": name},
+                  "spec": spec}
+
+    return experiment
