c1cc1d0ae7541aeb2b6d6c6859bffa1dedefaf22
DavidSpek <vanderspek.david@gmail.com>
2021-02-01 19:35:44 +0100

    check the status of a snapshot

--- backend/kale/common/snapshotutils.py
+++ backend/kale/common/snapshotutils.py
@@ -15,6 +15,7 @@
 import logging
 import random
 import string
+import time
 
 from kale.common import podutils, k8sutils
 
@@ -209,3 +210,31 @@ def delete_pvc_snapshot(snapshot_name):
         plural="volumesnapshots",
         name=snapshot_name)
     return
+
+
+def check_snapshot_status(snapshot_name):
+    """Check if volume snapshot is ready to use."""
+    log.info("Checking snapshot with snapshot name: %s", snapshot_name)
+    count = 0
+    max_count = 60
+    task = None
+    status = None
+    while status is not True and count <= max_count:
+        count += 1
+        try:
+            task = get_pvc_snapshot(snapshot_name=snapshot_name)
+            status = task['status']['readyToUse']
+            log.info(task)
+            log.info(status)
+            time.sleep(2)
+        except KeyError:
+            log.info("Snapshot resource %s does not seem to be ready",
+                     snapshot_name)
+            time.sleep(2)
+    if status is True:
+        log.info("Successfully created volume snapshot")
+    elif status is False:
+        raise log.info("Snapshot not ready (status: %s)", status)
+    else:
+        raise log.info("Unknown snapshot task status: %s", status)
+    return task
