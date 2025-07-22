13690ffb99fcfb4379c8acdfc10b7d5094911e31
Amrit27k <amritkmr4272@gmail.com>
2025-07-22 18:49:14 +0100

    cleaned the branch from frontend files

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-# sys.path.insert(0, 'D:\Projects\kale\kale\backend')
-# print(sys.path)
-# sys.path.remove('D:\\Projects\\kale')
-print(sys.path)
-from kale.processors import NotebookProcessor
-import kale
-print(kale.__file__)
-print("Kale package initialized with NotebookProcessor:", NotebookProcessor)
