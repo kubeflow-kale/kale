edbf292816786744587ca47cec23bbdaadbbb6b9
Amrit27k <amritkmr4272@gmail.com>
2025-08-26 02:22:04 +0100

    flake8 fix for kfputil

--- backend/kale/common/kfputils.py
+++ backend/kale/common/kfputils.py
@@ -157,7 +157,7 @@ def upload_pipeline(pipeline_package_path: str, pipeline_name: str,
                                  key=lambda v: v.created_at)
         delete_vid = sorted_versions[0].pipeline_version_id
         client.delete_pipeline_version(
-            pipeline_id=pipeline_id, 
+            pipeline_id=pipeline_id,
             pipeline_version_id=delete_vid
         )
         log.info("Deleted pipeline version with name '%s' and ID: %s",
