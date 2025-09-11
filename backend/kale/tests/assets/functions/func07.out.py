0eac5e7e322194cd60c3b8c4ac031f0cd59ae9a7
Stefano Fioravanzo <stefano.fioravanzo@gmail.com>
2025-09-11 14:37:02 +0200

    chore: remove leftover folder

--- backend/kale/tests/assets/functions/func07.out.py
+++
@@ -1,8 +0,0 @@
-def final_auto_snapshot():
-
-    from kale.common import rokutils as _kale_rokutils
-    _rok_snapshot_task = _kale_rokutils.snapshot_pipeline_step(
-        "test",
-        "final_auto_snapshot",
-        "/path/to/nb",
-        before=False)
