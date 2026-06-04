d19e7a45883f7c921d72de5f5278ba5c7001c340
jesuino <359820+jesuino@users.noreply.github.com>
2026-06-04 20:31:44 +0000

    Bump dev version to 2.1.2a1
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- kale/__init__.py
+++ kale/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2.1.1"
+__version__ = "2.1.2a1"
 
 from typing import Any, NamedTuple
 
